f76b9f7e1c0c136a4ab431e8400a742dfc85cfda
VictorRodri <victorexperto@hotmail.com>
2017-02-24 11:16:14 +0100

    incluyendo en el repositorio los ficheros del calculo del centroide de los heliostatos

---
+++ calculo del centroide.py
@@ -0,0 +1,241 @@
+import numpy as np
+import cv2
+import time
+import math
+import heliostatosMetodos as met
+import argparse #necesario para introducir valores por parametro
+import urllib.request
+from matplotlib import pyplot as plt
+
+HTTP_response = urllib.request.urlopen('http://www.hpca.ual.es/~vruiz/images/un_heliostato0500.png')
+arr = np.asarray(bytearray(HTTP_response.read()), dtype=np.uint8)
+BRG = cv2.imdecode(arr,-1)
+img = cv2.cvtColor(BRG, cv2.COLOR_BGR2GRAY)
+plt.imshow(img, cmap="gray")
+
+ret,thresh = cv2.threshold(img,127,255,0)
+
+plt.imshow(thresh, cmap="gray")
+
+now = time.time()
+im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+M = cv2.moments(contours[0])
+print ("Time =", time.time() - now)
+
+M = cv2.moments(contours[0])
+print(M)
+
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+
+print(cx, cy)
+
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+plt.scatter([cx],[cy])
+plt.imshow(img, cmap="gray")
+
+# INICIO LEYENDA
+# Las variables de control empezaran por VC_
+# Las variables que pueden ser modificadas por el usuario empezaran por VU_
+# Las variables que se usen como contadores empezaran por C_
+# Las variables que solo se modifican 1 vez durante todo el programa empezará por E_ (estatico)
+# FIN LEYENDA
+
+#INSTRUCCIONES PARA MODIFICAR EL COMANDO QUE LANZA LA APLICACION
+#_____ prog -> para cambiar el nombre del comando que lanza este programa.
+#_____description -> info sobre lo que hace el programa
+parser = argparse.ArgumentParser(prog="calibration",description="Process for identifying heliostats.")
+
+parser.add_argument("--numHeliostats",default="4",type=int,help="set the number of heliostats to calibrate at once")
+parser.add_argument("--pyrLevels",default="0",type=int, #nargs="?",
+                     help="set how many times the Pyramid method will be applied to source")
+args = parser.parse_args()
+
+# Variables obtenidas por el usuario
+cantHel = args.numHeliostats
+cantPyrD = args.pyrLevels
+
+# Cargamos el vídeo
+#camara = cv2.VideoCapture("Videos/video_paint3.mp4")
+camara = cv2.VideoCapture("C:/Users/Víctor/Desktop/CUARTO/practicasCurricularesUnia/varios_heliostatos.mp4")
+fondo = None # Inicializamos el primer frame a vacío. Nos servirá para obtener el fondo
+
+# Listas
+listaHel = []
+listaTamañoHel = []
+# Contadores
+contHel = 0
+iteraciones = 0
+contAñadir = 0
+cont2 = 0
+
+# Variables de control
+comenzarRegistroMovimiento = False # Comenzamos de nuevo a detectar mas manchas cuando hay mas de 1 proyeccion
+comenzarEstablecerFondo = False
+add1Mancha = False
+add2Mancha = False
+noEntrar1Mancha = False
+comprPAP = False
+
+#**************************
+#** EMPEZAMOS EL PROGRAMA**
+#**************************
+
+#ORDEN DE SALIDA:
+#3, 2, 4, 1
+
+while True:
+	hora1 = time.time()
+	# Obtenemos el frame
+	(grabbed, frame) = camara.read()
+ 	
+	# Si hemos llegado al final del vídeo salimos
+	if not grabbed:
+		break
+ 	
+ 	# Bucle para reducir la imagen tantas veces como queramos
+	for x in range(0,cantPyrD):
+ 		frame = cv2.pyrDown(frame)
+
+	# Convertimos a escala de grises
+	gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+ 	# Aplicamos suavizado para eliminar ruido
+	gris = cv2.GaussianBlur(gris, (21, 21), 0)
+
+	# Si todavía no hemos obtenido el fondo, lo obtenemos. Será el primer frame que obtengamos
+	if fondo is None:
+		fondo = gris
+		total = fondo.shape[0]*fondo.shape[1] #calculamos la altura y anchura
+		total = total/8
+		umbralFondo = met.umbralizar(gris)
+		continue 	
+
+	# Aplicamos un umbral
+	umbral = met.umbralizar(gris)
+
+	# Dilatamos el umbral para tapar agujeros
+	umbral = cv2.dilate(umbral, None, iterations = 2)
+
+	
+	# Calculo de la diferencia entre el fondo y el frame actual
+	resta = cv2.absdiff(fondo, gris)
+	umbral = met.umbralizar(resta)
+		
+	# Copiamos el umbral para detectar los contornos
+	contornosimg = umbral.copy()
+
+	# Buscamos contorno en la imagen. La variable contornosimg cambia! im = contornosimg
+	im, contornos, hierarchy = cv2.findContours(contornosimg,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+	
+	edges = cv2.Canny(umbral,255,255)
+
+	# Comprobamos que la lista este vacia. Si lo esta cambiamos la variable comproPAP a false para que cuando se vuelva a salir
+	# un trozo de mancha compruebe cual es y no tenga que hacer la comprobacion muchas veces
+	if met.listaVacia(contornos) == True and comprPAP == True:
+		comprPAP = False
+
+	# Recorremos todos los contornos encontrados
+	for c in contornos:
+		# Eliminamos los contornos más pequeño
+		contorno = cv2.contourArea(c)
+		
+		# Obtenemos el bounds del contorno, el rectángulo mayor que engloba al contorno
+		(x1, y1, w, h) = cv2.boundingRect(c) # w: es wigth y h: es heigh
+		
+		# Dibujamos el rectángulo del bounds
+		cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+		
+		# Si el contorno es tan pequeño suponemos es algo que no deberia estar...
+		if contorno <= 200:
+			continue
+		# Si cumple los requisitos suponemos que es movimiento. 
+		# Habria que poner una condicion de si se detecta el movimiento fuera de un cuadrado...
+		elif contorno > 200 and contorno<total and comenzarRegistroMovimiento == True:
+			# Hacemos una especie de contador para que solo lo haga 1 vez
+			if comprPAP == False:
+				# Calculamos el punto (x2,y2):
+				y2 = y1 + h
+				x2 = x1 + w
+				manchaNueva = edges[y1:y2, x1:x2]
+				pos = 0
+				pos, funciona = met.lorenzo(listaHel, manchaNueva)
+				if funciona!=False:
+					print("Hay movimiento, creemos que es el heliostato ",(pos+1))
+				#time.sleep(10)
+				if cont2==30 or funciona == False:
+					comprPAP = True
+					print("siguiente")
+					cont2=0
+					funciona = True
+				else:
+					cont2+=1
+
+		# Una vez hemos añadido las manchas establecemos el fondo. Damos 150 frame 
+		elif comenzarEstablecerFondo == True:
+			if cantHel == contHel:
+				#met.visualizarLista(listaHel)
+				if iteraciones==150:
+					fondo = gris # El nuevo fondo sera gris
+					umbralFondo = met.umbralizar(gris)
+					comenzarRegistroMovimiento = True
+				while(iteraciones<150):
+					iteraciones+=1
+					(grabbed, frame) = camara.read()
+		# Esta condicion se usa para ir añadiendo manchas.
+		elif comenzarRegistroMovimiento == False:
+			# La logica es la siguiente: en el momento en que haya un contorno y aparezca otro, añado el primer contorno a la lista.
+			if cantHel == contHel:
+				print("Comenzamos a comparar")
+				comenzarEstablecerFondo = True
+			elif len(contornos)>1:
+				if contAñadir == 10:
+					helCompr = []
+					for t in contornos:
+						(x1, y1, w, h) = cv2.boundingRect(t)
+						#calculamos el punto (x2,y2):
+						y2 = y1 + h
+						x2 = x1 + w
+						manchaNu = edges[y1:y2, x1:x2]
+						helCompr.append(manchaNu)
+				else:
+					contAñadir+=1
+				# Condicion para que no entre mas en la condicion de abajo, sino en la siguiente
+				if noEntrar1Mancha == True:
+					add1Mancha = True
+				else:
+					add2Mancha = True
+			# Una vez que ya se han juntado las 2 primeras manchas en 1, las añadimos a listaHel
+			elif len(contornos)==1 and add2Mancha == True:
+				listaHel.append(helCompr[1]) # En la posicion 1 esta la mancha que ya estaba antes
+				listaHel.append(helCompr[0]) # En la posicion 0 esta la mancha nueva
+				add2Mancha = False
+				noEntrar1Mancha = True
+				contHel += 2
+			# Este condicional solo añade una mancha
+			elif len(contornos)==1 and add1Mancha == True:
+				listaHel.append(helCompr[0])
+				add1Mancha = False
+				contHel += 1
+		
+	# Mostramos las imágenes de la cámara, el umbral y la resta
+	cv2.imshow("Camara", frame)
+	cv2.imshow("Edges", edges)
+	#cv2.imshow("Umbral", umbral)
+	cv2.imshow("Resta", resta)
+	
+
+	# Capturamos una tecla para salir
+	key = cv2.waitKey(1) & 0xFF
+	
+	# Tiempo de espera para que se vea bien
+	time.sleep(0.015)
+
+	# Si ha pulsado la letra s, salimos
+	if key == ord("s"):
+		break
+
+# Liberamos la cámara y cerramos todas las ventanas
+camara.release()
+cv2.destroyAllWindows()
