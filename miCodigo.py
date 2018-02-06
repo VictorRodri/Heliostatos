4965730356c5ff91c905695388afb41fc9117263
Victor Rodriguez <victorexperto@hotmail.com>
2018-02-06 22:12:59 +0100

    Mejorando el commit del pasado 27 de diciembre, el de leer y obtener las componentes RGB de cada pixel del heliostato central de cada fotograma del video de heliostatos. Concretamente, he agregado algunos calculos para obtener la energia del reflejo de la proyeccion del heliostato, indicando en formato codigo que la energia es el cuadrado de las componentes RGB.

--- miCodigo.py
+++ miCodigo.py
@@ -36,19 +36,20 @@ while True:
     
     # Aplicamos un umbral
     ret,thresh = cv2.threshold(img,127,255,0) # Parametros de este metodo: fuente, umbral, valor maximo, aplicar un tipo concreto de umbralizacion (0 porque no se desea hacer esto).
+    # La variable ‘ret’ de la linea de codigo anterior no es usada en este programa así que se puede ignorar, esto es debido a que no se está aplicando umbralización de Otsu.
     cv2.imshow("Camara2", thresh) # Mostrar video umbralizado en una ventana.
     cv2.waitKey(1) # El programa hara una pequena pausa (1 milisegundo) para que de tiempo a que se muestren los videos y fotogramas en las dos ventanas que se han creado en este codigo para tal fin.
 
-    now = time.time() # Tomar el tiempo actual.
+    # now = time.time() # Tomar el tiempo actual.
     # Parametros del siguiente metodo: Imagen umbralizada, devolver todos los contornos y crear una lista completa de jerarquia de familia, marcar la mínima cantidad de puntos (no todos)
-    # que forman (delimitan) la figura (heliostato).
+    # que forman (delimitan) la figura (heliostato). Argumentos que devolvera dicho metodo: imagen fuente (sobra), modo de devolucion del contorno, metodo de aproximacion del contorno (sobra).
     im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    M = cv2.moments(contours[0]) # Calcular los momentos del primer contorno, para cada fotograma del video.
+    M = cv2.moments(contours[0]) # Calcular los momentos del primer contorno, para cada fotograma del video. Los momentos permiten calcular el centro de masa del objeto, su area, etcetera.
     #print ("Time =", time.time() - now) # Restar el tiempo actual de esta linea menos el tomado 3 lineas antes en este codigo para calcular el tiempo de 'im2' y de los momentos.
     #print('Momentos: ', M)
     
     
-    if M['m00'] != 0:
+    if M['m00'] != 0: # Si el divisor es distinto de 0, hacer.
         #print("m00 es distinto de cero, cx y cy valen: ");
         # Centroides 'cx' y 'cy', y su forma de calcularlos.
         cx = int(M['m10']/M['m00'])
@@ -81,6 +82,11 @@ while True:
     # Area de varios contornos en un fotograma del video
     # Cada vez que se empiece a ejecutar el siguiente bucle 'for', se reestablece 'areaMayorDeTodas' a cero para evitar tomar accidentalmente el valor mayor de iteraciones anteriores a la actual.
     areaMayorDeTodas = 0
+
+    rTot = 0
+    gTot = 0
+    bTot = 0
+    
     # Recorrer todos los contornos (siguiente bucle 'for') de cada fotograma del video (bucle 'while' ejecutandose actualmente).
     # El numero maximo de contornos en cada fotograma del video es variable, y por eso se pone 'len(contours)',
     # para recorrer desde el contorno 0 hasta el numero maximo de contornos del fotograma del video en cuestion.
@@ -99,13 +105,24 @@ while True:
                 for yAux in range(y, h+1):
                     # Obtener las componentes RGB de las coordenadas (pixel) XY
                     b, g, r = frame[xAux, yAux]
-                    print('pixel:', b, g, r)
+                    print("xAux, yAux", xAux, yAux)
 
-                    # Sumar las componentes RGB
-                    #sumaRGB = b + g + r
-                    #print('sumaRGB:', sumaRGB)
+                    print("RGB", r, g, b)
+                    # Cada componente RGB se eleva al cuadrado.
+                    r2 = pow(r, r) # Tambien vale r**r en lugar de pow(r, r)
+                    g2 = pow(g, g)
+                    b2 = pow(b, b)
+                    print("RGB2", r2, g2, b2)
 
-                    print("Coordenadas contorno:", xAux, yAux)
+                    # Realizar la sumatoria RGB (cada componente por separado) de todos los pixeles del contorno principal.
+                    rTot += r2
+                    gTot += g2
+                    bTot += b2
+                    print("SumaRGBsepar", rTot, gTot, bTot)
+
+                    # Realizar la sumatoria RGB (esta vez las tres componentes al mismo tiempo) de todos los pixeles del contorno principal.
+                    sumaRGB = rTot+gTot+bTot
+                    print("SumaRGBtotal", sumaRGB)
 
         # print("Coordenadas rectangulos verdes:", x, y, w, h)
                 
@@ -137,12 +154,12 @@ while True:
 
 
     # Grabar TXT
-    def grabartxt():
-        archi=open('datos.txt','a') # abrir y crear el archivo de texto 'datos.txt' en el mismo directorio donde se esta ejecutando este programa
-        archi.write(repr(areaMayorDeTodas)) # repr convierte de float a string. Escribir en el TXT el area mas grande de todas las areas (contornos) detectadas en cada fotograma actual del video
-        archi.write('\n') # retorno de carro, para que cada valor escrito en el archivo de texto este en una unica linea
-        archi.close() # tras realizar todas las operaciones anteriores, cerrar el archivo de texto
-    grabartxt()
+    # def grabartxt():
+        # archi=open('datos.txt','a') # abrir y crear el archivo de texto 'datos.txt' en el mismo directorio donde se esta ejecutando este programa
+        # archi.write(repr(areaMayorDeTodas)) # repr convierte de float a string. Escribir en el TXT el area mas grande de todas las areas (contornos) detectadas en cada fotograma actual del video
+        # archi.write('\n') # retorno de carro, para que cada valor escrito en el archivo de texto este en una unica linea
+        # archi.close() # tras realizar todas las operaciones anteriores, cerrar el archivo de texto
+    # grabartxt()
     
 
     # Si tras analizar todos los contornos del fotograma actual, se detecto o no el contorno principal (el primer contorno de todos), se mostrara por consola si lo detecto o no correctamente.
